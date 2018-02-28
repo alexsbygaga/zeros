50bc5a5bb0a5b083f60a78ad25a6268ab1c12299
alexsbygaga <alexsbygaga@gmail.com>
2018-03-01 01:14:20 +0300

    src/index.js fix crash bug in first version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 module.exports = function getZerosCount(number) {
-    /**
+        /**
+    var res = Math.floor(number/5);
+    var result = Math.floor(number/5);
+    while(res != 1){
+        res = Math.floor(res/5);
+        result += res;
+        I dont know why, but this is crash the system, but it seems good and it work.
+    };**/
     var first = Math.floor(number/5);
     var second = Math.floor(first/5);
     var third = Math.floor(second/5);
@@ -10,14 +17,9 @@ module.exports = function getZerosCount(number) {
     var eights = Math.floor(seventh/5);
     var ninth = Math.floor(eights/5);
     var tens = Math.floor(ninth/5);
-    var el = Math.floor(tens/5); **/
-    var res = Math.floor(number/5);
-    var result = Math.floor(number/5);
-    while(res != 1){
-        res = Math.floor(res/5);
-        result += res;
-    };
+    var el = Math.floor(tens/5); 
 
-    return result;
+
+    return first+second+third+fourth+fifth+sixth+seventh+eights+ninth+tens+el;
     
 }
