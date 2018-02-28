1e56fa436d817228b06e6a4bd0c78b9e096ce9f3
alexsbygaga <alexsbygaga@gmail.com>
2018-03-01 01:09:32 +0300

    src/index.js Add functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    /**
+    var first = Math.floor(number/5);
+    var second = Math.floor(first/5);
+    var third = Math.floor(second/5);
+    var fourth = Math.floor(third/5);
+    var fifth = Math.floor(fourth/5);
+    var sixth = Math.floor(fifth/5);
+    var seventh = Math.floor(sixth/5);
+    var eights = Math.floor(seventh/5);
+    var ninth = Math.floor(eights/5);
+    var tens = Math.floor(ninth/5);
+    var el = Math.floor(tens/5); **/
+    var res = Math.floor(number/5);
+    var result = Math.floor(number/5);
+    while(res != 1){
+        res = Math.floor(res/5);
+        result += res;
+    };
+
+    return result;
+    
 }
